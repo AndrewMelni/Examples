3e5e22a1bad5492eaff425c84b5bb66a70416e22
Andrei <Andrei-melnikov86@rambler.ru>
2022-12-18 00:36:01 +0300

    поис элемента массива

---
+++ Example010_PoiskElementa/Program.cs
@@ -0,0 +1,17 @@
+﻿// Поиск определённого элемента массива
+int[] array={1,21,13,4,5,6,4,7,8,9};
+
+int n = array.Length;
+int find=4;
+
+int index=0;
+
+while(index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
